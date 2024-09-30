8b1787e9c02d1667ad78e684e472926455bab8ef
Rose <skillwarzaking@gmail.com>
2024-09-30 19:17:48 +0200

    Patched avoidance message dump.
    Patched the 'Squad #{0} driver issued avoidance path' console message dumps.

---
+++ src/actor/ai/AiIssueAvoidancePathPatcher.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using rose.row.util;
+using UnityEngine;
+
+namespace rose.row.actor.ai
+{
+    [HarmonyPatch(typeof(AiActorController), nameof(AiActorController.IssueAvoidancePath))]
+    internal class AiIssueAvoidancePathPatcher
+    {
+        public static void prefix(AiActorController __instance, VehicleParticleManager.AvoidancePath path)
+        {
+            __instance.setAvoidancePassSegment(path.thruB - path.thruA);
+            Vector3 position = __instance.actor.seat.vehicle.GetPosition();
+
+            __instance.GotoDirect(new Vector3[]
+            {
+                path.thruA,
+                path.thruB,
+                position + __instance.getAvoidancePassSegment() * 10f
+            }, false);
+
+            __instance.setAvoidancePassPoint(path.passPoint);
+            __instance.setIsOnAvoidancePath(true);
+        }
+    }
+}
