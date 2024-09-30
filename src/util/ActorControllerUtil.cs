2b42bacd3ccfc706e777b38d77caf769611595d6
Rose <skillwarzaking@gmail.com>
2024-09-30 19:17:06 +0200

    Added utility for private AAIC fields.
    Added utility for accessing and setting private fields in the AiActorController.

--- src/util/ActorControllerUtil.cs
+++ src/util/ActorControllerUtil.cs
@@ -1,4 +1,7 @@
-﻿namespace rose.row.util
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace rose.row.util
 {
     public static class ActorControllerUtil
     {
@@ -17,5 +20,39 @@
         /// This is the same as doing <see cref="ActorController.actor.dead"/>.
         /// </remarks>
         public static bool dead(this ActorController controller) => controller.actor.dead;
+
+        #region ai
+
+        public static Vector3 getAvoidancePassSegment(this AiActorController aiActorController)
+        {
+            return (Vector3) Traverse.Create(aiActorController).Field("avoidancePassSegment").GetValue();
+        }
+
+        public static void setAvoidancePassSegment(this AiActorController aiActorController, Vector3 value)
+        {
+            Traverse.Create(aiActorController).Field("avoidancePassSegment").SetValue(value);
+        }
+
+        public static Vector3 getAvoidancePassPoint(this AiActorController aiActorController)
+        {
+            return (Vector3) Traverse.Create(aiActorController).Field("avoidancePassPoint").GetValue();
+        }
+
+        public static void setAvoidancePassPoint(this AiActorController aiActorController, Vector3 value)
+        {
+            Traverse.Create(aiActorController).Field("avoidancePassPoint").SetValue(value);
+        }
+
+        public static bool getIsOnAvoidancePath(this AiActorController aiActorController)
+        {
+            return (bool) Traverse.Create(aiActorController).Field("isOnAvoidancePath").GetValue();
+        }
+
+        public static void setIsOnAvoidancePath(this AiActorController aiActorController, bool value)
+        {
+            Traverse.Create(aiActorController).Field("isOnAvoidancePath").SetValue(value);
+        }
+
+        #endregion
     }
 }
