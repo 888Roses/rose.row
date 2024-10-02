4b35dcb1361aac6ccefa5f7d2d432dc635a847a7
Rose <skillwarzaking@gmail.com>
2024-10-02 09:37:29 +0200

    Removed weird actor animation on hit

---
+++ src/actor/behaviour/DisableActorAnimationWhenHurtPatcher.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+
+namespace rose.row.actor.behaviour
+{
+    [HarmonyPatch(typeof(Actor), nameof(Actor.Hurt))]
+    internal class DisableActorAnimationWhenHurtPatcher
+    {
+        [HarmonyPrefix]
+        static bool prefix(Actor __instance, float x)
+        {
+            if (__instance.fallenOver || __instance.dead)
+            {
+                return false;
+            }
+
+            //__instance.animator.SetFloat(Actor.ANIM_PAR_HURT_X, x);
+            //__instance.animator.SetTrigger(Actor.ANIM_PAR_HURT);
+            //__instance.hurtAction().Start();
+
+            return false;
+        }
+    }
+}
