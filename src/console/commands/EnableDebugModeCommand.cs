14031858b0acb2b475aacd78d4317dd04b779565
Rose <skillwarzaking@gmail.com>
2024-10-03 16:35:45 +0200

    Added a debug command to enable debug mode.

---
+++ src/console/commands/EnableDebugModeCommand.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace rose.row.console.commands
+{
+    public class EnableDebugModeCommand : AbstractConsoleCommand
+    {
+        public override string root => "debug";
+        public override string description => "Enables debug mode.";
+
+        public override void execute()
+        {
+            Traverse.Create(GameManager.instance).Method("InitializeIngameDebugGizmos").GetValue();
+        }
+    }
+}
