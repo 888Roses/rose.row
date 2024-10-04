ed5ce5c11072433490cccf3be0a02b6c9b8742c9
Rose <skillwarzaking@gmail.com>
2024-10-04 10:59:34 +0200

    Fixed warning messages when loading mod

---
+++ src/mods/SetSourceModPatcher.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace rose.row.mods
+{
+    [HarmonyPatch(typeof(ModContentInformation))]
+    [HarmonyPatch("sourceMod", MethodType.Setter)]
+    internal class SetSourceModPatcher
+    {
+        [HarmonyPrefix]
+        static bool prefix(ModContentInformation __instance, ModInformation value)
+        {
+            Traverse.Create(__instance).Field("_sourceMod").SetValue(value);
+            //__instance.serializableSourceModReference = new SerializedModReference(__instance._sourceMod);
+            __instance.serializableSourceModReference = ScriptableObject.CreateInstance<SerializedModReference>();
+            __instance.serializableSourceModReference.modInformation = (ModInformation) Traverse.Create(__instance).Field("_sourceMod").GetValue();
+            return false;
+        }
+    }
+}
