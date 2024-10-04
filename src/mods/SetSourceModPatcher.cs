a659bd9d41b673f67e3b8b3e66c0d32c9100f680
Rose <skillwarzaking@gmail.com>
2024-10-04 11:03:30 +0200

    Update SetSourceModPatcher.cs

--- src/mods/SetSourceModPatcher.cs
+++ src/mods/SetSourceModPatcher.cs
@@ -11,7 +11,6 @@ namespace rose.row.mods
         static bool prefix(ModContentInformation __instance, ModInformation value)
         {
             Traverse.Create(__instance).Field("_sourceMod").SetValue(value);
-            //__instance.serializableSourceModReference = new SerializedModReference(__instance._sourceMod);
             __instance.serializableSourceModReference = ScriptableObject.CreateInstance<SerializedModReference>();
             __instance.serializableSourceModReference.modInformation = (ModInformation) Traverse.Create(__instance).Field("_sourceMod").GetValue();
             return false;
