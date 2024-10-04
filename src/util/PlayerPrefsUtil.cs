3563a5772538fc052158a3eb13e30d28eca3cd87
Rose <skillwarzaking@gmail.com>
2024-10-04 16:13:14 +0200

    Create PlayerPrefsUtil.cs

---
+++ src/util/PlayerPrefsUtil.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace rose.row.util
+{
+    public static class PlayerPrefsUtil
+    {
+        // Using a string and not an int32 since a one char string is lighter (2b) than an int32 (4b).
+        public static void setBool(string key, bool value) => PlayerPrefs.SetString(key, value ? "t" : "f");
+        public static bool getBool(string key) => PlayerPrefs.GetString(key) == "t";
+        public static bool getBool(string key, bool defaultValue)
+            => PlayerPrefs.GetString(key, defaultValue ? "t" : "f") == "t";
+    }
+}
