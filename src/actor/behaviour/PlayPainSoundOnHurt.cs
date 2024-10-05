3a0539d1f1e7f68a4bbe2b6449fbbc4581672791
Rose <skillwarzaking@gmail.com>
2024-10-05 13:51:08 +0200

    Fixed pain sound only playing when hurting actor

--- src/actor/behaviour/PlayPainSoundOnHurt.cs
+++ src/actor/behaviour/PlayPainSoundOnHurt.cs
@@ -10,6 +10,21 @@ namespace rose.row.actor.behaviour
         public static void subscribeToInitializationEvents()
         {
             Events.onActorHurt.after += onActorHurt;
+            Events.onActorDie.after += onActorDie;
+        }
+
+        private static void onActorDie(Actor actor, DamageInfo info, bool arg3)
+        {
+            if (actor == null)
+            {
+                Debug.LogError($"Cannot play pain sound for null actor.");
+                return;
+            }
+
+            if (!info.isPlayerSource || !actor.aiControlled || info.sourceActor.team == actor.team)
+                return;
+
+            Audio.play(AudioRegistry.hitPain.random().get());
         }
 
         private static void onActorHurt(Actor actor, DamageInfo info)
